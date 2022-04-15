99d9cf8475aed05ec64c6199936f34e5c71db1a3
Angie G <angieg@Angies-MBP.fios-router.home>
2022-04-15 11:37:51 -0400

    Convert Price & mySortFunction

--- solution.js
+++ solution.js
@@ -1,3 +1,4 @@
+const { isArrayBuffer } = require("util/types");
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 const swap = (arr, index1, index2) => {
@@ -118,21 +119,22 @@ const sortProductPriceD = (someProducts) => {
 
 // sort products by price, then by name, ascending order
 const sortProducsPriceNameA = (someProducts) => {
-  // let swapped = true;
-  // while (swapped) {
-  //   swapped = false;
-  //   for (let i = 0; i < someProducts.length - 1; i++) {
-  //     if (someProducts[i].price > someProducts[i + 1].price) {
-  //       swap(someProducts, i, i + 1);
-  //       swapped = true;
-  //     } else if (someProducts[i].price === someProducts[i + 1].price) {
-  //       if (someProducts[i].name > someProducts[i + 1].name)
-  //         swap(someProducts, i, i + 1);
-  //       swapped = true;
-  //     }
-  //   }
-  // }
-  // return someProducts;
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < someProducts.length - 1; i++) {
+      if (someProducts[i].price > someProducts[i + 1].price) {
+        swap(someProducts, i, i + 1);
+        swapped = true;
+      } else if (someProducts[i].price === someProducts[i + 1].price) {
+        if (someProducts[i].name > someProducts[i + 1].name) {
+          swap(someProducts, i, i + 1);
+          swapped = true;
+        }
+      }
+    }
+  }
+  return someProducts;
 };
 
 // sort catArt by designedBy in ascending order:
@@ -151,7 +153,37 @@ const catArtSortDesginedByA = (catArt) => {
 };
 
 // sort catArt by price in ascending order
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (catArt) => {
+  //
+  let swapped = true;
+  // while(swapped) will only run the loop if it's true,
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < catArt.length - 1; i++) {
+      // console.log(catArt[i].price);
+
+      if (convertPrice(catArt[i].price) > convertPrice(catArt[i + 1].price)) {
+        swap(catArt, i, i + 1);
+        swapped = true;
+      }
+    }
+  }
+  return catArt;
+};
+
+let convertPrice = (price) => {
+  // price strings convert to Number
+  let finalPrice = Number(price);
+  // check if the finalPrice is a number data type  Ex: ♇♇3, which is a string
+  // Number is checking to see if it's an actual number (===)
+  if (Number.isNaN(finalPrice)) {
+    // ♇♇3, which is a string
+    // finalPrice = Number(price.slice(2)) * 10;
+    return Number(price.slice(2)) * 10;
+  }
+  // console.log(typeof finalPrice);
+  return finalPrice;
+};
 
 // Create your own sort function
 // it should sort in ascending order
@@ -161,7 +193,21 @@ const catArtSortByPriceA = () => {};
 // or try to implement merge sort
 // or look up another common sort algorithm (i.e. quicksort, ) and try your own implementation
 // Bonus add another argument that would allow the function to be used for ascending or descending order
-const mySortFunction = () => {};
+
+const mySortFunction = (arr) => {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        //if true, swap the numbers
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  }
+  return arr;
+};
 
 module.exports = {
   sortNumsA,
