22729d93ce6b2549f962bf3564a7f3d22361e776
John Prado <jcprado20@icloud.com>
2022-10-23 15:17:07 -0400

    adds some methods

--- solution.js
+++ solution.js
@@ -1,34 +1,173 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i] > array[i + 1]){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i] < array[i + 1]){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i] > array[i + 1]){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i].toLowerCase() < array[i + 1].toLowerCase()){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
+const sortProductNamesA = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i].name.toLowerCase() > array[i + 1].name.toLowerCase()){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i].price > array[i + 1].price){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort products by price, descending order
-const sortProductPriceD = () => {};
+const sortProductPriceD = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i].price < array[i + 1].price){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i].price > array[i + 1].price){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+
+  }
+  return array
+};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i].designedBy > array[i + 1].designedBy){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const convertToMeowa = (price) => {
+  if(Number.isNaN(price)){
+    price = price.slice(2)
+    return (Number(price) * 10)
+  }
+}
+
+const catArtSortByPriceA = (array) => {
+  let unswapped = true;
+
+  while(unswapped){
+    unswapped = false;
+    for(let i = 0; i < array.length-1; i++){
+      
+      if(convertToMeowa(array[i].price) > convertToMeowa(array[i + 1].price)){
+        [array[i], array[i +1]] = [array[i+1], array[i]]
+        unswapped = true
+      }
+    }
+  }
+  return array
+};
 
 // Create your own sort function
 // it should sort in ascending order
@@ -52,4 +191,5 @@ module.exports = {
   catArtSortDesginedByA,
   catArtSortByPriceA,
   mySortFunction,
+  convertToMeowa
 };
