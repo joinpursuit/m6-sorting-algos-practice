f21f31e45d8ddd798376f1917fe9bec3f9e8773c
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2022-10-23 14:10:47 -0400

    Update sorting.test.js
    Changes '10' to be at end of array line 27

--- _tests_/sorting.test.js
+++ _tests_/sorting.test.js
@@ -24,7 +24,7 @@ const {
 describe("Sorting Practice", () => {
   test("Sort numbers in ascending order", () => {
     expect(sortNumsA(someNums)).toStrictEqual([
-      0, 1, 10, 2, 3, 4, 5, 6, 7, 8, 9,
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     ]);
   });
   test("Sort numbers in descending order", () => {
