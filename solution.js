68527c569a7f66778cb35f04a4cb1be076ec3b4d
Tom LaTulipe <tlatulipe@gmail.com>
2022-10-23 16:01:30 -0400

    about 1/2 way done

--- solution.js
+++ solution.js
@@ -1,19 +1,86 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = (numbers) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < numbers.length - 1; i++) {
+      if (numbers[i] > numbers[i + 1]) {
+        [numbers[i], numbers[i + 1]] = [numbers[i + 1], numbers[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return numbers;
+};
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (numbers) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < numbers.length - 1; i++) {
+      if (numbers[i] < numbers[i + 1]) {
+        [numbers[i], numbers[i + 1]] = [numbers[i + 1], numbers[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return numbers;
+};
 
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = (words) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < words.length - 1; i++) {
+      if (words[i][0] > words[i + 1][0]) {
+        [words[i], words[i + 1]] = [words[i + 1], words[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return words;
+};
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = (words) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < words.length - 1; i++) {
+      if (words[i].toLowerCase() < words[i + 1].toLowerCase()) {
+        // if the word is "less than" the next word...
+        [words[i], words[i + 1]] = [words[i + 1], words[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return words;
+};
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
+const sortProductNamesA = (someProducts) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < someProducts.length - 1; i++) {
+      if (
+        someProducts[i].name.toLowerCase() >
+        someProducts[i + 1].name.toLowerCase()
+      ) {
+        [someProducts[i], someProducts[i + 1]] = [
+          someProducts[i + 1],
+          someProducts[i],
+        ];
+        unsorted = true;
+      }
+    }
+  }
+  return someProducts;
+};
 
 // sort products by price, ascending order
 const sortProductPriceA = () => {};
@@ -28,7 +95,12 @@ const sortProducsPriceNameA = () => {};
 const catArtSortDesginedByA = () => {};
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (catArt) => {
+  let unsorted = true;
+  while (unsorted) {
+    
+  }
+};
 
 // Create your own sort function
 // it should sort in ascending order
