904ab93709d19ff9e818299e674efced5573c6e8
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-10-23 21:33:04 -0400

    6 tests passed

--- solution.js
+++ solution.js
@@ -1,22 +1,106 @@
-const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
+const { catArt, someNums, someProducts, someWords } = require('./data/data.js');
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
-
+const sortNumsA = (someNums) => {
+  do {
+    swapped = false;
+    for (let i = 0; i < someNums.length; i++) {
+      if (someNums[i] > someNums[i + 1]) {
+        [someNums[i], someNums[i + 1]] = [someNums[i + 1], someNums[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return someNums;
+};
+//console.log(sortNumsA(someNums));
 // sort numbers in descending order
-const sortNumsD = () => {};
-
+const sortNumsD = (someNums) => {
+  do {
+    swapped = false;
+    for (let i = 0; i < someNums.length; i++) {
+      if (someNums[i] < someNums[i + 1]) {
+        [someNums[i], someNums[i + 1]] = [someNums[i + 1], someNums[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return someNums;
+};
+//console.log(sortNumsD(someNums));
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
-
+const sortWordsA = (someWords) => {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < someWords.length - 1; i++) {
+      if (someWords[i] > someWords[i + 1]) {
+        [someWords[i], someWords[i + 1]] = [someWords[i + 1], someWords[i]];
+        swapped = true;
+      }
+    }
+  }
+  {
+    return someWords;
+  }
+};
+//console.log(sortWordsA(someWords));
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = (someWords) => {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < someWords.length - 1; i++) {
+      if (someWords[i].toUpperCase() < someWords[i + 1].toUpperCase()) {
+        [someWords[i], someWords[i + 1]] = [someWords[i + 1], someWords[i]];
+        swapped = true;
+      }
+    }
+  }
 
+  return someWords;
+};
+//console.log(sortWordsD(someWords));
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
-
+const sortProductNamesA = (someProducts) => {
+  do {
+    swapped = false;
+    for (let i = 0; i < someProducts.length - 1; i++) {
+      if (
+        someProducts[i].name.toUpperCase() >
+        someProducts[i + 1].name.toUpperCase()
+      ) {
+        [someProducts[i], someProducts[i + 1]] = [
+          someProducts[i + 1],
+          someProducts[i],
+        ];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  {
+    return someProducts;
+  }
+};
+console.log(sortProductNamesA(someProducts));
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = (someProducts) => {
+  do {
+    swapped = false;
+    for (let i = 0; i < someProducts.length - 1; i++) {
+      if (someProducts[i].price > someProducts[i + 1].price) {
+        [someProducts[i], someProducts[i + 1]] = [
+          someProducts[i + 1],
+          someProducts[i],
+        ];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  {
+    return someProducts;
+  }
+};
 
 // sort products by price, descending order
 const sortProductPriceD = () => {};
