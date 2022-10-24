ed7191716e701fa0a815310284a01163aa664656
TaryneLeach <taryneleach@gmail.com>
2022-10-23 22:33:12 -0400

    passing 3 so far

--- solution.js
+++ solution.js
@@ -1,34 +1,83 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = (nums) => {
+  let swapping = true
+  while (swapping) {
+    swapping = false
+    for (let n = 0; n < nums.length - 1; n++) {
+      if (nums[n] > nums[n + 1]) {
+       [nums[n], nums[n + 1]] = [nums[n + 1], nums[n]]
+       swapping = true
+      }
+    }
+  }
+  return nums
+};
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (nums) => {
+  let swapping = true
+  while (swapping) {
+    swapping = false
+    for (let n = 0; n < nums.length -1; n++) {
+      if (nums[n+1] > nums[n]) {
+        [nums[n], nums[n+1]] = [nums[n+1], nums[n]]
+        swapping = true
+      }
+    }
+  }
+  return nums;
+};
 
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = (words) => {
+  let swapping = true
+  while (swapping) {
+    swapping = false
+    for (let n = 0; n < words.length - 1; n++) {
+      if (words[n] > words[n+1]) {
+        [words[n], words[n+1]] = [words[n+1], words[n]]
+        swapping = true
+      }
+    }
+  }
+  return words;
+};
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+// this one is case sensitive but not the ascending problem?
+const sortWordsD = (words) => {
+  let swapping = true
+  while (swapping) {
+    swapping = false
+    for (let n = 0; n < words.length -1; n++) {
+      if (words[n+1].toUpperCase() > words[n].toUpperCase()) {
+        [words[n], words[n+1]] = [words[n+1], words[n]]
+        swapping = true
+      }
+    }
+  }
+  return words
+};
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
+const sortProductNamesA = (name) => {};
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = (price) => {};
 
 // sort products by price, descending order
-const sortProductPriceD = () => {};
+const sortProductPriceD = (price) => {};
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (price, name) => {};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = (cat) => {};
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (cat) => {};
 
 // Create your own sort function
 // it should sort in ascending order
