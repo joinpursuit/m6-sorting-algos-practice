bf34dd538e33c01be73385b2c2086c7618e69ffa
lily08 <liliangelxd@gmail.com>
2022-10-25 09:52:54 -0400

    saved

--- solution.js
+++ solution.js
@@ -1,34 +1,87 @@
-const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
+const { catArt, someNums, someProducts, someWords } = require('./data/data.js')
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = (arr) => {
+  return arr.sort()
+}
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (arr) => {
+  return arr.sort((a, b) => b - a)
+}
 
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = (arr) => {
+  return arr.sort()
+}
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = (array) => {
+  return array.sort(function (a, b) {
+    if (a.toLowerCase() < b.toLowerCase()) {
+      return 1
+    }
+    if (a.toLowerCase() > b.toLowerCase()) {
+      return -1
+    }
+    return 0
+  })
+}
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
+const sortProductNamesA = (array) => {
+  return array.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1
+    }
+    if (a.name > b.name) {
+      return 1
+    }
+    return 0
+  })
+}
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = (array) => {
+return array.sort((a,b)=> a.price -b.price)
+}
 
 // sort products by price, descending order
-const sortProductPriceD = () => {};
+const sortProductPriceD = (array) => {
+return array.sort((a, b) => b.price - a.price)
+}
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (array) => {
+  return array.sort((a, b) => a.price - b.price)
+}
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = (array) => {
+return array.sort(function (a, b) {
+  if (a.designedBy < b.designedBy) {
+    return -1
+  }
+  if (a.designedBy > b.designedBy) {
+    return 1
+  }
+  return 0
+})
+}
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (array) => {
+  return array.sort((a, b) =>{
+   
+  if (a.designedBy < b.designedBy) {
+    return -1
+  }
+  if (a.designedBy > b.designedBy) {
+    return 1
+  }
+  return 0
+})
+}
 
 // Create your own sort function
 // it should sort in ascending order
@@ -38,7 +91,21 @@ const catArtSortByPriceA = () => {};
 // or try to implement merge sort
 // or look up another common sort algorithm (i.e. quicksort, ) and try your own implementation
 // Bonus add another argument that would allow the function to be used for ascending or descending order
-const mySortFunction = () => {};
+const mySortFunction = (arr) => {
+    let didISwap = true
+    while (didISwap) {
+      didISwap = false
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          didISwap = true
+          let temp = arr[i + 1]
+          arr[i + 1] = arr[i]
+          arr[i] = temp
+        }
+      }
+    }
+    return arr
+}
 
 module.exports = {
   sortNumsA,
@@ -52,4 +119,4 @@ module.exports = {
   catArtSortDesginedByA,
   catArtSortByPriceA,
   mySortFunction,
-};
+}
