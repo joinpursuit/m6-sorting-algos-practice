9ddfcaa1e17ea095a9d3945bd304729eb8af151c
T.K <nique@niques-air.myfiosgateway.com>
2022-10-24 20:36:55 -0400

    passing all

--- solution.js
+++ solution.js
@@ -147,12 +147,36 @@ const catArtSortDesginedByA = (array) => {
 // sort catArt by price
 const catArtSortByPriceA = (array) => {
   let unsorted = true;
+  let converterA = 0;
+  let converterB = 0;
   while (unsorted) {
     unsorted = false;
 
     for (let i = 0; i < array.length - 1; i++) {
       // console.log(array[i].price)
-      if (array[i].price > array[i + 1].price) {
+      if (typeof array[i].price === "string") {
+        // console.log(!Number(array[i].price))
+        if (Number(array[i].price)) {
+          converterA = Number(array[i].price);
+        } else if (!Number(array[i].price)) {
+          converterA = Number(array[i].price.slice(2)) * 10;
+        } 
+      } else {
+        converterA = array[i].price;
+      }
+
+      if (typeof array[i + 1].price === "string") {
+        // console.log(array[i].price.slice(2))
+        if (Number(array[i + 1].price)) {
+          converterB = Number(array[i + 1].price);
+        } else if (!Number(array[i].price)) {
+          converterB = Number(array[i + 1].price.slice(2)) * 10;
+        } 
+      } else {
+        converterB = array[i + 1].price;
+      }
+
+      if (converterA > converterB) {
         [array[i], array[i + 1]] = [array[i + 1], array[i]];
         unsorted = true;
       }
@@ -169,7 +193,20 @@ const catArtSortByPriceA = (array) => {
 // or try to implement merge sort
 // or look up another common sort algorithm (i.e. quicksort, ) and try your own implementation
 // Bonus add another argument that would allow the function to be used for ascending or descending order
-const mySortFunction = () => {};
+const mySortFunction = (array) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return array;
+};
 
 module.exports = {
   sortNumsA,
